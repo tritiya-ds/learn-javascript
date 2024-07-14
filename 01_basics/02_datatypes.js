9ffd900e0b08fd5b78831cf9f616d6e6736bea7b
Tritiya Dey Sarkar <168653192+tritiya-ds@users.noreply.github.com>
2024-07-14 12:36:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+// code readability should be high//
+console.log(3+3)
+console.log("tritiya")
+
+let name = "tritiya" //string datatype
+let age = 13 //number
+let isLoggedIn = true //boolean datatype
+let state ;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "tritiya") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof 34) //number
+
