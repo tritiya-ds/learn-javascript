367c83decd6c01521117e5a2b083cab312fe5823
Tritiya Dey Sarkar <168653192+tritiya-ds@users.noreply.github.com>
2024-07-18 15:54:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" ==> 33
+// "33abc" ==> NaN (not a number)
+// null ==> 0
+// undefined ==> NaN
+// true/false ==> 1/0
+// ram ==> NaN
+
+// let isLoggedIn = 2
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1,2, "ram" ==> true
+// 0,"" ==> false
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber); //string
+
+
+
+
+
