5dc5861c0127d724e4ccd9e5837690ae0b2ed257
Tritiya Dey Sarkar <168653192+tritiya-ds@users.noreply.github.com>
2024-07-19 05:11:38 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);//true
+// console.log(2>=1);//true
+// console.log(2==1);//false
+// console.log(2!=1);//true
+
+// console.log("2">1);//true
+
+// console.log(null>0); //false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+
+// console.log( undefined>0);//false
+// console.log( undefined==0);//false
+// console.log( undefined>=0);//false
+// console.log( undefined<0);//false
+
+// === --> it doesn't convert the "2" string datatype to number , in this case if both the datatype doesn't matches it simply prints "false", if matches then it prints "true"
+// console.log("2" === 2); //false
